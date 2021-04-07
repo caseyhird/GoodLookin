3a2d0d30d30a25492beefe7ee49427c63801838d
Casey Hird <crhird@g.clemson.edu>
2021-04-07 09:54:42 -0400

    added intent for text search from main to results

--- app/src/main/java/com/zybooks/goodlookin/MainActivity.java
+++ app/src/main/java/com/zybooks/goodlookin/MainActivity.java
@@ -2,13 +2,28 @@ package com.zybooks.goodlookin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText searchText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchText = findViewById(R.id.searchEditText);
     }
+
+    private void textSearchClick(View view) {
+        String searchVal = searchText.getText().toString();
+        Intent intent = new Intent(this, ResultsActivity.class);
+        intent.putExtra(ResultsActivity.EXTRA_SEARCH_VAL, searchVal);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/zybooks/goodlookin/ResultsActivity.java
+++ app/src/main/java/com/zybooks/goodlookin/ResultsActivity.java
@@ -2,13 +2,24 @@ package com.zybooks.goodlookin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
+    public static final String EXTRA_SEARCH_VAL = "search_string";
+    private String searchVal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        // When searching by text, get search string from intent
+        Intent intent = getIntent();
+        if (intent.getExtras().containsKey(EXTRA_SEARCH_VAL))
+            searchVal = intent.getStringExtra(EXTRA_SEARCH_VAL);
+        TextView t = findViewById(R.id.searchString);
+        t.setText(searchVal);
     }
 }
