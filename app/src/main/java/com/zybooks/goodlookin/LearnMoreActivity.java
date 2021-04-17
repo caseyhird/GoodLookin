4c909c77f7508904167d0e1e57aca94a5be819b0
Casey Hird <crhird@g.clemson.edu>
2021-04-17 15:48:46 -0400

    attempt shake reset in confirm and learn more

--- app/src/main/java/com/zybooks/goodlookin/LearnMoreActivity.java
+++ app/src/main/java/com/zybooks/goodlookin/LearnMoreActivity.java
@@ -2,9 +2,46 @@ package com.zybooks.goodlookin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 public class LearnMoreActivity extends AppCompatActivity {
+
+
+    private SensorManager mSensorManager;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
+
+    private final SensorEventListener mSensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta;
+            if (mAccel > 12) {
+                backToMain();
+                //Toast.makeText(getApplicationContext(), "Shake event detected", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+
+    private void backToMain() {
+        Intent confirm = new Intent(this, MainActivity.class);
+        startActivity(confirm);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
